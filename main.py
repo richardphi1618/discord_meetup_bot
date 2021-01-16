eaf2d1d44f1949621c8f6461d448a83c4e6486cd
richphi <richardphi1618@gmail.com>
2021-01-16 18:20:19 -0500

    giving it some structure / easier to read output

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import json
 
 response = r.get(mra._url("events"))
 print (response.json())
+
+for x in response.json():
+    print(f"Event: {x['name']} with the occurs on Date: {x['local_date']}\n Link: {x['link']}")
